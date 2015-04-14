6cd99e79996ae4f77709d8913874a8e36c968394
Dan Foulkes <danfoulkes@Dans-MBP.default>
2015-04-14 20:25:07 +0100

    updating GPIO services to get the current state of the lights

--- src/main/java/com/foulkes/lights/mvc/service/FileService.java
+++ src/main/java/com/foulkes/lights/mvc/service/FileService.java
@@ -1,10 +1,12 @@
 package com.foulkes.lights.mvc.service;
 
 import com.foulkes.lights.mvc.enums.Socket;
+import com.foulkes.lights.mvc.service.exception.StatusException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
 import org.springframework.util.FileCopyUtils;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Created by danfoulkes on 14/04/15.
@@ -14,9 +16,9 @@ public class FileService implements GPIOService {
     File on;
     File off;
     File socket1;
+    // Logger logger  = LogManager.getLogger(FileService.class);
 
-
-    public FileService(){
+    public FileService() {
         //load on and off files
         on = new File("/lights/outputFiles/on.txt");
         off = new File("/lights/outputFiles/off.txt");
@@ -25,9 +27,11 @@ public class FileService implements GPIOService {
 
     @Override
     public Boolean executeCommand(Socket socket, Boolean status) {
-        switch(socket) {
-            case SOC1: return sendToSocket1(status);
-            default: return true;
+        switch (socket) {
+            case SOC1:
+                return sendToSocket1(status);
+            default:
+                return true;
         }
     }
 
@@ -41,8 +45,7 @@ public class FileService implements GPIOService {
                 e.printStackTrace();
                 return true;
             }
-        }
-        else{
+        } else {
             try {
                 FileCopyUtils.copy(off, socket1);
                 return false;
@@ -51,5 +54,36 @@ public class FileService implements GPIOService {
                 return false;
             }
         }
+    }
+
+    public Boolean getLightStatus(Socket soc) throws StatusException {
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader (socket1));
+            String         line = null;
+            StringBuilder  stringBuilder = new StringBuilder();
+            String         ls = System.getProperty("line.separator");
+            Boolean status;
+            try {
+                while( ( line = reader.readLine() ) != null ) {
+                    stringBuilder.append(line);
+                }
+                String currentCommand = stringBuilder.toString();
+                if(currentCommand.compareToIgnoreCase("on") == 0){
+                    return true;
+                }
+                else if(currentCommand.compareToIgnoreCase("off") == 1){
+                    return false;
+                }
+            } catch (Exception e) {
+                return false;
+            } finally {
+                reader.close();
+            }
+        } catch (Exception e) {
+
+            throw new StatusException();
+        }
+        throw new StatusException();
     }
 }
