6ee0c610eea565e40c8873f5c48259d2ee2c7857
Dan Foulkes <danfoulkes@Dans-MBP.default>
2015-10-28 19:03:29 +0000

    fixed webservice package

--- lightService/src/main/java/com/foulkes/lights/mvc/endpoints/UpdateParent.java
+++ lightService/src/main/java/com/foulkes/lights/mvc/endpoints/UpdateParent.java
@@ -3,21 +3,18 @@ package com.foulkes.lights.mvc.endpoints;
 import com.foulkes.lights.mvc.setup.AppConfig;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 /**
  * Created by danfoulkes on 24/10/2015.
  */
-@Component
-public class UpdateParent {
+public class UpdateParent{
 
     Logger logger = Logger.getLogger(UpdateParent.class);
 
     @Autowired
     private AppConfig appConifg;
 
-
-    public void call() {
+    public void execute() {
         logger.info("Job was called");
     }
 }
