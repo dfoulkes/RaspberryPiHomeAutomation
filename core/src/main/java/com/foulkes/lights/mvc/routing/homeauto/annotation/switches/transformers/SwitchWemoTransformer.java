58907dc97b0e16c85bbc95863122a5bba3317aa6
danfoulkes <danfoulkes@gmail.com>
2016-03-26 10:11:42 +0000

    updating the config, need to fix the wemo interface which seems to have broken since correctly the parnet name

--- core/src/main/java/com/foulkes/lights/mvc/routing/homeauto/annotation/switches/transformers/SwitchWemoTransformer.java
+++ core/src/main/java/com/foulkes/lights/mvc/routing/homeauto/annotation/switches/transformers/SwitchWemoTransformer.java
@@ -1,14 +1,10 @@
 package com.foulkes.lights.mvc.routing.homeauto.annotation.switches.transformers;
 
-import com.foulkes.homeAutomation.service.wemo.execution.Command;
-import com.foulkes.homeAutomation.service.wemo.execution.body.CommandEnum;
-import com.foulkes.homeAutomation.service.wemo.execution.xml.GetInsightParamsResponse;
-import com.foulkes.homeAutomation.service.wemo.execution.xml.SetBinaryStateResponse;
-import com.foulkes.lights.common.enums.EventState;
+
+
 import com.foulkes.lights.common.events.ComponentEvent;
 import com.foulkes.lights.mvc.routing.homeauto.annotation.payloads.Response;
-import com.foulkes.lights.mvc.routing.homeauto.annotation.switches.payloads.SwitchResponse;
-import com.foulkes.lights.mvc.routing.homeauto.annotation.switches.payloads.WemoResponse;
+
 import org.springframework.integration.annotation.MessageEndpoint;
 import org.springframework.integration.annotation.Transformer;
 import org.springframework.messaging.Message;
@@ -26,33 +22,34 @@ public class SwitchWemoTransformer implements SwitchTransformer_ {
     @Override
     @Transformer(inputChannel="switch_wemo_insight", outputChannel="processResponse")
     public Response processMessage(Message<ComponentEvent> message) throws IOException {
-        //TODO communicate with WEMO device
-        EventState event = message.getPayload().getEventState();
-        Command command;
-        switch (event){
-            case ON: command = new Command(CommandEnum.BINARY_ON); break;
-            case OFF: command = new Command(CommandEnum.BINARY_OFF); break;
-            default: command = new Command(CommandEnum.USAGE);
-        }
-        return generateResponse(command.execute(message.getPayload().getIp(), message.getPayload().getAddress()));
+//        //TODO communicate with WEMO device
+//        EventState event = message.getPayload().getEventState();
+//        Command command;
+//        switch (event){
+//            case ON: command = new Command(CommandEnum.BINARY_ON); break;
+//            case OFF: command = new Command(CommandEnum.BINARY_OFF); break;
+//            default: command = new Command(CommandEnum.USAGE);
+//        }
+//        return generateResponse(command.execute(message.getPayload().getIp(), message.getPayload().getAddress()));
+    return null;
     }
 
 
 
-    private Response generateResponse(com.foulkes.homeAutomation.service.wemo.execution.xml.Response wemo){
+    private Response generateResponse(Response wemo){
 
-        if( wemo instanceof GetInsightParamsResponse){
-            GetInsightParamsResponse insight  = (GetInsightParamsResponse)wemo;
-            return new WemoResponse(insight.getInsightParams(), insight.getState(), insight.getLastchange(), insight.getLengthOfTimeOnInSecs(),
-                    insight.getLengthOfTimeOnToday(), insight.getLengthOfTimeOnTotal(), insight.getTimeperiod(), insight.getUnknownValue(), insight.getCurrentmw(),
-                    insight.getTodaymw(), insight.getTotalmw(), insight.getPowerthreshold());
-        }
-        else if(wemo instanceof SetBinaryStateResponse){
-            SetBinaryStateResponse leaver = (SetBinaryStateResponse) wemo;
-            SwitchResponse response = new SwitchResponse();
-            response.setEventState((leaver.getState()? EventState.ON : EventState.OFF));
-            return response;
-        }
+//        if( wemo instanceof GetInsightParamsResponse){
+//            GetInsightParamsResponse insight  = (GetInsightParamsResponse)wemo;
+//            return new WemoResponse(insight.getInsightParams(), insight.getState(), insight.getLastchange(), insight.getLengthOfTimeOnInSecs(),
+//                    insight.getLengthOfTimeOnToday(), insight.getLengthOfTimeOnTotal(), insight.getTimeperiod(), insight.getUnknownValue(), insight.getCurrentmw(),
+//                    insight.getTodaymw(), insight.getTotalmw(), insight.getPowerthreshold());
+//        }
+//        else if(wemo instanceof SetBinaryStateResponse){
+//            SetBinaryStateResponse leaver = (SetBinaryStateResponse) wemo;
+//            SwitchResponse response = new SwitchResponse();
+//            response.setEventState((leaver.getState()? EventState.ON : EventState.OFF));
+//            return response;
+//        }
 
         return null;
     }
